d825144da84661e8ac5aeb65b2674687a8aa59b3
Your Name <you@example.com>
2022-09-06 10:19:05 +0000

    fix add scripts

--- mkrump.sh
+++ mkrump.sh
@@ -3,3 +3,4 @@ rm -rf rumpkit
 git clone https://github.com/Infinitybotlist/rumpkit
 cp -rf rumpkit/* $1
 rm -rf $1/{README.md,mkrump.sh}
+rm -rf rumpkit
--- updrump.sh
+++ updrump.sh
@@ -2,4 +2,5 @@ add_templates.py
 rm -rf rumpkit
 git clone https://github.com/Infinitybotlist/rumpkit
 cp -rf rumpkit/* $1
-rm -rf $1/README.md
+rm -rf $1/{README.md,mkrump.sh}
+rm -rf rumpkit
