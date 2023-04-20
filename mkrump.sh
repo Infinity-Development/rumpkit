ec739013b90f0cc7e24be6c5f64b1a5c90c3c09f
Rootspring <cheesycod@outlook.com>
2023-04-20 12:34:41 +0530

    Update mkrump.sh

--- mkrump.sh
+++ mkrump.sh
@@ -1,6 +1,6 @@
 mkdir -p $1
 rm -rf rumpkit
-git clone https://github.com/Infinitybotlist/rumpkit
+git clone https://github.com/Infinity-Development/rumpkit
 cp -rf rumpkit/* $1
 rm -rf $1/{README.md,mkrump.sh}
 rm -rf rumpkit
